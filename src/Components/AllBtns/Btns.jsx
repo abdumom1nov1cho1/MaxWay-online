8852407493d6572470caa58849cbd3fbd7589e0e
abdumom1nov1cho1 <116268072+abdumom1nov1cho1@users.noreply.github.com>
2023-09-09 23:10:32 +0500

    ...

--- src/Components/AllBtns/Btns.jsx
+++ src/Components/AllBtns/Btns.jsx
@@ -16,32 +16,34 @@ const Btns = () => {
     dots: true,
     infinite: false,
     speed: 500,
-    slidesToShow: 7.5,
+    slidesToShow: 6.5,
     slidesToScroll: 4,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 5.5,
+          slidesToShow: 4,
           slidesToScroll: 5,
           infinite: true,
-          dots: false
+          dots: true
         }
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 4.5,
+          slidesToShow: 3.5,
           slidesToScroll: 4,
-          initialSlide: 2
+          initialSlide: 2,
+          dots: false
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 3.5,
-          slidesToScroll: 2
+          slidesToShow: 2.25,
+          slidesToScroll: 2,
+          dots: false
         }
       }
     ]
